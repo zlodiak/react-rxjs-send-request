4eb3aaa886847cc19d7cf0612c869947089c5eb3
kalinin <prozaik81-2@yandex.ru>
2020-12-26 15:14:08 +0300

    change initialstate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { BehaviorSubject, combineLatest } from 'rxjs/index';
+import axios from 'axios';
+import { BehaviorSubject, combineLatest, timer } from 'rxjs';
+import { flatMap, map, debounce, filter } from 'rxjs/operators';
 
-const withObservableStream = (observable, triggers) => Component => {
+const withObservableStream = (observable, triggers, initialState) => Component => {
   return class extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        ...initialState,
+      };
+    }
+
     componentDidMount() {
       this.subscription = observable.subscribe(newState =>
         this.setState({ ...newState }),
@@ -29,6 +39,7 @@ const SUBJECT = {
 const App = ({
   query = '',
   subject,
+  stories = [],
   onChangeQuery,
   onSelectSubject,
 }) => (
@@ -54,19 +65,50 @@ const App = ({
     </div>
 
     <p>{`http://hn.algolia.com/api/v1/${subject}?query=${query}`}</p>
+
+    <ul>
+      {stories.map(story => (
+        <li key={story.objectID}>
+          <a href={story.url || story.story_url}>
+            {story.title || story.story_title}
+          </a>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
 const query$ = new BehaviorSubject('react');
 const subject$ = new BehaviorSubject(SUBJECT.POPULARITY);
+const queryForFetch$ = query$.pipe(
+  debounce(() => timer(1000)),
+  filter(query => query !== ''),
+);
+const fetch$ = combineLatest(subject$, queryForFetch$).pipe(
+  flatMap(([subject, query]) =>
+    axios(`http://hn.algolia.com/api/v1/${subject}?query=${query}`),
+  ),
+  map(result => result.data.hits),
+);
 
 export default withObservableStream(
-  combineLatest(subject$, query$, (subject, query) => ({
-    subject,
-    query,
-  })),
+  combineLatest(
+    subject$,
+    query$,
+    fetch$,
+    (subject, query, stories) => ({
+      subject,
+      query,
+      stories,
+    }),
+  ),
   {
     onChangeQuery: value => query$.next(value),
     onSelectSubject: subject => subject$.next(subject),
+  },
+  {
+    query: 'react',
+    subject: SUBJECT.POPULARITY,
+    stories: [],
   },
 )(App);
